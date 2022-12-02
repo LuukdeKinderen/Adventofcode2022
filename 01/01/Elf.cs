f0e821e8e26d13ece77ae2a9520094ed4ae25c1c
luukdekinderen <luukdekinderen@outlook.com>
2022-12-02 15:50:44 +0100

    Day one

---
+++ 01/01/Elf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    public class Elf
+    {
+        public List<int> snacks = new List<int>();
+
+        public int TotalCalories => snacks.Sum();
+
+        public void AddSnack(string snack)
+        {
+            snacks.Add(int.Parse(snack));
+        }
+    }
+}
---
+++ 01/01/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _01
+{
+    internal class Program
+    {
+        public static List<Elf> elves = new List<Elf>();
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Input.txt");
+
+
+            Elf current = new Elf();
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    elves.Add(current);
+                    current = new Elf();
+                }
+                else
+                {
+                    current.AddSnack(line);
+                }
+            }
+
+            foreach (var elf in elves)
+            {
+                PrintElf(elf);
+            }
+
+            var maxElf = elves.MaxBy(e => e.TotalCalories);
+            Console.WriteLine("Elf with max calories:");
+            PrintElf(maxElf);
+
+
+            var topThreeElves = elves.OrderByDescending(e => e.TotalCalories).Take(3);
+
+            Console.WriteLine("Top three elves: ");
+            foreach (var elf in topThreeElves)
+            {
+                PrintElf(elf);
+            }
+
+            Console.WriteLine("Total of top three elves: ");
+            Console.WriteLine(topThreeElves.Sum(e => e.TotalCalories));
+        }
+
+
+        private static void PrintElf(Elf elf)
+        {
+            Console.WriteLine($"elf {elves.IndexOf(elf)}");
+            Console.Write("\t");
+            foreach (var snack in elf.snacks)
+            {
+                Console.Write(snack + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"\tTotal: {elf.TotalCalories}");
+            Console.WriteLine();
+        }
+
+    }
+}
