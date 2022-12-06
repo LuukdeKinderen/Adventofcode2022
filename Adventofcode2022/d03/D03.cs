df696b8434fc2d78494a70bbea9aab448eb50227
luukdekinderen <luukdekinderen@outlook.com>
2022-12-06 21:23:16 +0100

    Add day three second part

--- Adventofcode2022/d03/D03.cs
+++ Adventofcode2022/d03/D03.cs
@@ -2,9 +2,16 @@
 {
     public class D03
     {
-        static int totalValue = 0;
         public static void Start()
         {
+            PartOne();
+            PartTwo();
+        }
+
+        private static void PartOne()
+        {
+            int totalValue = 0;
+
             string[] lines = File.ReadAllLines("D03.txt");
             foreach (string line in lines)
             {
@@ -18,6 +25,41 @@
                 int value = Value(character);
                 Console.WriteLine(character + " " + value);
                 totalValue += value;
+            }
+            Console.WriteLine(totalValue);
+        }
+        private static void PartTwo()
+        {
+            string[] lines = File.ReadAllLines("D03.txt");
+
+            int totalValue = 0;
+
+            int lineCount = 0;
+            while (lineCount < lines.Length)
+            {
+                string[] group = new string[]
+                {
+                    lines[lineCount++],
+                    lines[lineCount++],
+                    lines[lineCount++],
+                };
+
+                foreach (string line in group)
+                {
+                    Console.WriteLine(line);
+                }
+                int groupValue = 0;
+                foreach(char c in group[0])
+                {
+                    if (group[1].Contains(c) && group[2].Contains(c))
+                    {
+                        groupValue = Value(c);
+                        break;
+                    }
+                }
+                totalValue += groupValue;
+                Console.WriteLine();
+
             }
             Console.WriteLine(totalValue);
         }
