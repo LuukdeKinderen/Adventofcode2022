a8c701af98dcf459c037ae0760edde0047fc0c0d
luukdekinderen <luukdekinderen@outlook.com>
2022-12-21 17:01:23 +0100

    Dag 6

--- Adventofcode2022/Adventofcode2022/Program.cs
+++ Adventofcode2022/Adventofcode2022/Program.cs
@@ -3,6 +3,7 @@ using d02;
 using d03;
 using d04;
 using d05;
+using d06;
 
 namespace Adventofcode2022
 {
@@ -14,7 +15,8 @@ namespace Adventofcode2022
             //D02.Start();
             //D03.Start();
             //D04.Start();
-            D05.Start();
+            //D05.Start();
+            D06.Start();
         }
     }
 }
---
+++ Adventofcode2022/d06/D06.cs
@@ -0,0 +1,33 @@
+﻿namespace d06
+{
+    public class D06
+    {
+        public static void Start()
+        {
+            string line = File.ReadAllText("D06.txt");
+
+            Console.WriteLine($"Part 1: {IndexWhereThereAreNumberOfUniqueCharacters(line, 4)}");
+            Console.WriteLine($"Part 2: {IndexWhereThereAreNumberOfUniqueCharacters(line, 14)}");
+
+        }
+
+        private static int IndexWhereThereAreNumberOfUniqueCharacters(string input, int amount)
+        {
+            int index = 0;
+            foreach (char streamChar in input)
+            {
+                var streamPiece = input.Skip(index).Take(amount).ToArray();
+
+                if (streamPiece.Distinct().Count() == amount)
+                {
+                    break;
+                }
+
+                index++;
+            }
+            return index + amount;
+
+        }
+
+    }
+}
